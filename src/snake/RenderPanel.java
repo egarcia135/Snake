aba66b67174a5da5a1038e4676585375ae9dc273
Eduardo Garcia <eduardo@Eduardos-Air.hsd1.ca.comcast.net>
2019-09-08 20:51:18 -0700

    Added Key Controls to Entity

--- src/snake/RenderPanel.java
+++ src/snake/RenderPanel.java
@@ -2,20 +2,26 @@ package snake;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 
 import javax.swing.JPanel;
 
 public class RenderPanel extends JPanel {
 	
-	public static int curColor = 0;
-
+	public static Color green = new Color(1666073);
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(new Color(curColor));
+		g.setColor(green);
 		g.fillRect(0, 0, 800, 700);
-		curColor++;
+		Snake snake = Snake.snake;
+		g.setColor(Color.RED);
+		for(Point point : snake.snakeParts) {
+			g.fillRect(point.x * Snake.SCALE, point.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
+		}
+		g.fillRect(snake.head.x * Snake.SCALE, snake.head.y * Snake.SCALE, Snake.SCALE, Snake.SCALE);
+	
 		
 	}
 	
--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
 import java.awt.Toolkit;
@@ -11,7 +13,7 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class Snake implements ActionListener {
+public class Snake implements ActionListener, KeyListener {
 	
 	public JFrame jframe;
 	public RenderPanel renderPanel;
@@ -20,8 +22,7 @@ public class Snake implements ActionListener {
 	
 	public ArrayList<Point> snakeParts = new ArrayList<Point>();
 	
-	public int ticks = 0, direction = DOWN, score;
-	
+	public int ticks = 0, direction = DOWN, score, tailLength; 
 	public Point head, cherry;
 	
 	public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
@@ -38,10 +39,16 @@ public class Snake implements ActionListener {
 		jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.height / 2 - jframe.getHeight() / 2 );
 		jframe.add(renderPanel = new RenderPanel());
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.addKeyListener(this);
 		head = new Point(0,0);
-		timer.start();
 		random = new Random();
 		cherry = new Point(dim.width/SCALE, dim.height/SCALE);
+		
+		for(int i = 0; i < tailLength; i++) {
+			snakeParts.add(new Point(head.x, head.y));
+			
+		}
+		timer.start();
 	}
 	
 	public static void main(String[] args) {
@@ -52,45 +59,75 @@ public class Snake implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		renderPanel.repaint();
 		ticks++;
+		
 		if(ticks % 10 == 0 && head != null && over != true) {
-			head = snakeParts.get(snakeParts.size() - 1);
+			snakeParts.add(new Point(head.x, head.y));
 			if(direction == UP) 
 				if(head.y - 1 > 0)
-					snakeParts.add(new Point(head.x, head.y - 1));
+					head = new Point(head.x, head.y - 1);
 				else
 					over = true;
 			if(direction == DOWN)
 				if(head.y + 1 < dim.height/SCALE)
-					snakeParts.add(new Point(head.x, head.y + 1));
+					head = new Point(head.x, head.y + 1);
 				else
 					over = true;
 			if(direction == LEFT)
 				if(head.x - 1 > 0)
-					snakeParts.add(new Point(head.x - 1, head.y));
+					head = new Point(head.x - 1, head.y);
 				else
 					over = true;
 			if(direction == RIGHT)
 				if(head.x + 1 < dim.width / SCALE)
-					snakeParts.add(new Point(head.x + 1, head.y));
+					head = new Point(head.x + 1, head.y);
 				else
 					over = true;
-			snakeParts.remove(0);
+			
+			
+			snakeParts.add(new Point(head.x, head.y));
+//			for(int i = 0; i < tailLength; i++) {
+//				snakeParts.remove(i);
+//				
+//			}
 			if(cherry != null) {
 				if(head.equals(cherry)) {
-					score++;
+					score+=10;
+					tailLength++;
 					cherry.setLocation(dim.width/ SCALE, dim.height/SCALE);
-				}
-				
+				}	
 			}
-		}
-		
-		
+		}	
 	}
 	
 	
 	public void updateSnake() {
 		snake = new Snake();
 		
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int i = e.getKeyCode();
+		if(i == KeyEvent.VK_A && direction != RIGHT)
+			direction = LEFT;
+		if(i == KeyEvent.VK_D && direction != LEFT)
+			direction = RIGHT;
+		if(i == KeyEvent.VK_W && direction != DOWN)
+			direction = UP;
+		if(i == KeyEvent.VK_S && direction != UP)
+			direction = DOWN;
+	
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 		
 	}
 	
